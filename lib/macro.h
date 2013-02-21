6e087bff8b250bf56e289420146ae429c12b8646
Justin Gottula <justin@jgottula.com>
2013-02-21 02:42:33 -0800

    lib: remove unnecessary SWAP macro

--- lib/macro.h
+++ lib/macro.h
@@ -4,11 +4,6 @@
 
 #define CEIL(_x, _step) ((_x) == 0 ? 0 : ((((_x) - 1) / (_step)) + 1))
 
-#define SWAP(_a, _b) do { \
-		typeof(_a) _temp_##_a_##_b = (_a); (_a) = (_b); \
-		(_b) = (_temp_##_a_##_b); \
-	} while (0);
-
 #define STRIFY(_s) _STRIFY(_s)
 #define _STRIFY(_s) #_s
 
