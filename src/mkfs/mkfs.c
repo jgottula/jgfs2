56a181d5bbd39cc193a7a28dd4ddb0490c9edf1f
Justin Gottula <justin@jgottula.com>
2013-02-23 20:30:32 -0800

    mkfs: remove data zap; allocate 14 boot sectors, not 16

--- src/mkfs/mkfs.c
+++ src/mkfs/mkfs.c
@@ -16,11 +16,10 @@ int main(int argc, char **argv) { // void
 	strcpy(param.label, "justix");
 	uuid_generate(param.uuid);
 	param.total_sect = 0;
-	param.boot_sect  = 16;
+	param.boot_sect  = 14;
 	param.blk_size   = 0;
 	param.zap_vbr    = true;
 	param.zap_boot   = true;
-	param.zap_data   = true;
 	
 	jgfs2_new(argv[1], &mount_opt, &param);
 	
