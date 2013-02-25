92f1f09387cae982ede7f585eb033fd32ae09df4
Justin Gottula <justin@jgottula.com>
2013-02-24 22:39:02 -0800

    lib: upgrade the dummy allocation algorithm

--- lib/extent.c
+++ lib/extent.c
@@ -8,8 +8,12 @@ uint32_t ext_alloc(uint32_t len) {
 	/* traverse the ext tree horizontally and find a free extent that is
 	 * large enough */
 	
+	static uint32_t alloc_ptr = 100;
+	
 	TODO("remove dummy code");
-	return 100;
+	return alloc_ptr++;
 }
 
-
+/* TODO: look into lazy allocation features of linux for when we have to get
+ * zeroed extents (holes) for the library user (or will we just use the
+ * read(2)/write(2) convention of filling a user buffer?) */
