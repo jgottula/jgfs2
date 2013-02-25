d9f05d340c0eff231e4361a2a3ac9dcff964da4b
Justin Gottula <justin@jgottula.com>
2013-02-24 22:40:08 -0800

    lib/tree: move struct inode_item into item.h

--- lib/tree/item.h
+++ lib/tree/item.h
@@ -6,6 +6,29 @@
 #include "key.h"
 
 
+enum item_key {
+	KEY_INODE = 0x01,
+};
+
+
+struct __attribute__((__packed__)) inode_item {
+	uint32_t i_attr;  // file attributes
+	uint16_t i_mode;  // type and permissions
+	
+	uint16_t i_uid;   // owner
+	uint16_t i_gid;   // group
+	
+	uint32_t i_size;  // length in bytes of file content
+	
+	int64_t  i_atime; // access time
+	int64_t  i_ctime; // creation time
+	int64_t  i_mtime; // modification time
+	
+	uint32_t i_nlink; // hard link ref count
+	
+	uint32_t i_gen;   // generation
+};
+
 struct item_data {
 	uint32_t len;
 	void *data;
