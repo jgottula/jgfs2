ea0d70334204420ba6f67a6c340cd689ac71c418
Justin Gottula <justin@jgottula.com>
2013-03-11 16:02:15 -0700

    test: add more random functions

--- src/test/rand.c
+++ src/test/rand.c
@@ -13,16 +13,8 @@ uint32_t rand32(void) {
 	return (uint32_t)mrand48();
 }
 
-uint64_t rand64(void) {
-	uint64_t top    = (uint32_t)mrand48();
-	uint64_t bottom = (uint32_t)mrand48();
-	
-	return (top << 32) | bottom;
-}
-
+/* random in range [0, max]: stackoverflow.com/questions/2999075 */
 uint32_t rand32_range(uint32_t max) {
-	/* stackoverflow.com/questions/2999075 */
-	
 	if (max == UINT32_MAX) {
 		return rand32();
 	}
@@ -37,9 +29,15 @@ uint32_t rand32_range(uint32_t max) {
 	return result;
 }
 
-uint64_t rand64_range(uint64_t max) {
-	/* stackoverflow.com/questions/2999075 */
+uint64_t rand64(void) {
+	uint64_t top    = (uint32_t)mrand48();
+	uint64_t bottom = (uint32_t)mrand48();
 	
+	return (top << 32) | bottom;
+}
+
+/* random in range [0, max]: stackoverflow.com/questions/2999075 */
+uint64_t rand64_range(uint64_t max) {
 	if (max == UINT64_MAX) {
 		return rand64();
 	}
@@ -54,13 +52,33 @@ uint64_t rand64_range(uint64_t max) {
 	return result;
 }
 
-/* inside-out Fisher-Yates shuffle */
-void rand_permute_init(uint32_t *arr, size_t cnt) {
+/* fill array with random values in range [0, max] */
+void rand32_fill_range(uint32_t *arr, size_t cnt, uint32_t max) {
+	while (cnt-- != 0) {
+		*(arr++) = rand32_range(max);
+	}
+}
+
+/* standard Fisher-Yates shuffle */
+void rand32_permute(uint32_t *arr, size_t cnt) {
+	for (size_t i = cnt - 1; i >= 1; --i) {
+		size_t j = rand32_range(i);
+		
+		/* swap arr[i] with arr[j] */
+		uint32_t temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
+
+/* inside-out Fisher-Yates shuffle, initialized to values from [0, cnt) */
+void rand32_permute_init(uint32_t *arr, size_t cnt) {
 	arr[0] = 0;
 	
 	for (size_t i = 1; i < cnt; ++i) {
 		size_t j = rand32_range(i);
 		
+		/* swap and initialize */
 		arr[i] = arr[j];
 		arr[j] = i;
 	}
--- src/test/rand.h
+++ src/test/rand.h
@@ -10,12 +10,15 @@
 
 
 uint32_t rand32(void);
-uint64_t rand64(void);
-
 uint32_t rand32_range(uint32_t max);
+
+uint64_t rand64(void);
 uint64_t rand64_range(uint64_t max);
 
-void rand_permute_init(uint32_t *arr, size_t cnt);
+void rand32_fill_range(uint32_t *arr, size_t cnt, uint32_t max);
+
+void rand32_permute(uint32_t *arr, size_t cnt);
+void rand32_permute_init(uint32_t *arr, size_t cnt);
 
 
 #endif
